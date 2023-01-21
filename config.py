c24897efa564a0683e7ec65cd689d0ad1bb01712
Adam Ahmed <avahmed@ualberta.ca>
2023-01-21 15:26:03 -0700

    Add buffer size to config

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ current_time = datetime.now().strftime("%Y-%m-%d")
 LOG_LEVEL = logging.INFO
 LOG_FILENAME = f'webserver-{current_time}.log'
 LOG_DATEFMT = '%Y-%m-%dT%H:%M:%S'
-LOG_FORMAT = '%(asctime)s %(name)s %(message)s'
+LOG_FORMAT = '%(asctime)s %(levelname)s:%(name)s %(message)s'
 
 # Webserver
 BIND_HOST = 'localhost'
 BIND_PORT = 8080
+
+BUFFER_SIZE = 1024
